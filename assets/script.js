2f5c644ff79a5d20485ce5c6c775aafaec430b29
DPLong <dplong742@gmail.com>
2022-06-08 12:52:42 -0400

    cleanup and container

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,6 @@ var inputForm = $("#word-search-form");
 var input = $("#q");
 var submitBtn = $("#submit-btn");
 
-//init function
-// function init() {    
-// }
-
 inputForm.on("submit", function (event) {
     event.preventDefault();
     //Oxford API info
@@ -21,26 +17,34 @@ inputForm.on("submit", function (event) {
     $.ajax(dictionaryURL).done(function (data) {
         console.log(data);
         $(dictionaryBox).empty();
+        $(dictionaryBox).removeClass("d-none");
+        $(dictionaryBox).addClass("d-block");
 
+        var h2El = $("<h2>");
+        $(h2El).text("Your Word!");
+        dictionaryBox.append(h2El);
         // For loop goes here
         for (var result of data.results) {
             console.log(result);
-            var h2El = $("<h2>");
-            h2El.addClass("card p-3 bg-light text-dark");
-            $(h2El).text(result.id);
-            dictionaryBox.append(h2El);
+            var dictContainer = $("<article>");
+            dictContainer.addClass("card p-3 my-2 bg-light text-dark");
+            dictionaryBox.append(dictContainer);
+
+            var wordEl = $("<p>");
+            $(wordEl).text(result.id);
+            dictContainer.append(wordEl);
 
             var defEl = $("<p>");
-            defEl.addClass("card p-3 bg-light text-dark");
+            // defEl.addClass("card p-3 bg-light text-dark");
             $(defEl).text(result.lexicalEntries[0].entries[0].senses[0].definitions);
-            dictionaryBox.append(defEl);
+            dictContainer.append(defEl);
 
             var etyEl = $("<p>");
             $(etyEl).text(result.lexicalEntries[0].entries[0].etymologies);
-            if ($(etyEl).text.length > 2 ){
-                etyEl.addClass("card p-3 bg-light text-dark");
-                dictionaryBox.append(etyEl);
-            } 
+            if ($(etyEl).text.length > 0) {
+                // etyEl.addClass("card p-3 bg-light text-dark");
+                dictContainer.append(etyEl);
+            }
         }
     });
 
@@ -48,25 +52,18 @@ inputForm.on("submit", function (event) {
     $.ajax(giphyURL).done(function (data) {
         console.log(data);
         $(gifBox).empty();
+        $(gifBox).removeClass("d-none");
+        $(gifBox).addClass("d-block");
 
-        // For loop goes here
-            console.log(data.data.url);
-            var imgEl = $("<iframe>");
-            $(imgEl).attr("src", data.data.embed_url);
-            imgEl.addClass("card p-3 bg-light text-dark");
-            gifBox.append(imgEl);
+        //Create image element
+        console.log(data.data.url);
+        var headEl = $("<h2>");
+        $(headEl).text("Your Word as a Fun GIF!");
+        gifBox.append(headEl);
 
-            // var defEl = $("<p>");
-            // defEl.addClass("card p-3 bg-light text-dark");
-            // $(defEl).text(result.lexicalEntries[0].entries[0].senses[0].definitions);
-            // gifBox.append(defEl);
-
-            // var etyEl = $("<p>");
-            // etyEl.addClass("card p-3 bg-light text-dark");
-            // $(etyEl).text(result.lexicalEntries[0].entries[0].etymologies);
-            // gifBox.append(etyEl);
+        var imgEl = $("<iframe>");
+        $(imgEl).attr("src", data.data.embed_url);
+        imgEl.addClass("card p-3 bg-light text-dark");
+        gifBox.append(imgEl);
     });
-})
-
-//Runs the functions
-// init();
+});
